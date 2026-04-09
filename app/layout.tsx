028a47bc4a5f1824b5e86067f6ab955f6552a323
Dokyung An <tea-co@naver.com>
2026-04-09 16:42:20 +0900

    구글 태그 매니저

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,6 +90,17 @@ export default function RootLayout({
   return (
     <html lang="ko" className="h-full scroll-smooth">
       <head>
+        {/* Google Tag Manager — 공식 스니펫(head 최상단). @next/third-parties 대신 태그매니저 안내 그대로 사용 */}
+        {/* eslint-disable-next-line @next/next/next-script-for-ga -- GTM 설치 가이드 인라인 스크립트 */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WNWK5MMZ');`,
+          }}
+        />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
@@ -99,6 +110,16 @@ export default function RootLayout({
       <body
         className={`${notoSans.variable} ${notoSerif.variable} min-h-full flex flex-col antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WNWK5MMZ"
+            height={0}
+            width={0}
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
         {children}
       </body>
     </html>
